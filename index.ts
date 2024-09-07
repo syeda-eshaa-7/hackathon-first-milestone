9a637c051ac73bd7dc9022b1bea1864155153ae5
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-09-08 03:12:23 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,12 @@
+const toggleSkillsBtn = document.getElementById("toggle-skills-btn")!;
+const skillsList = document.getElementById("skills-list")!;
+
+toggleSkillsBtn.addEventListener("click", () => {
+    if (skillsList.style.display === "none") {
+        skillsList.style.display = "block";
+        toggleSkillsBtn.innerHTML = "Hide Skills";
+    } else {
+        skillsList.style.display = "none";
+        toggleSkillsBtn.innerHTML = "Show Skills";
+    }
+});
